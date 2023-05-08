3845c948ca3dcbba261d881bc7a9aa04c1ef4675
Harsh <harshkeshari100@gmail.com>
2023-05-08 19:05:26 +0530

    added home component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
-        <Routes></Routes>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
